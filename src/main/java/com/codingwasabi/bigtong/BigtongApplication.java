471ab050d865ede5121e40d36eef49464a9bceb9
daehwan7989@naver.com <daehwan7989@naver.com>
2021-04-18 18:50:43 +0900

    # build : 프로젝트 빌드, 라이브러리 추가

---
+++ src/main/java/com/codingwasabi/bigtong/BigtongApplication.java
@@ -0,0 +1,13 @@
+package com.codingwasabi.bigtong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigtongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigtongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingwasabi/bigtong/BigtongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingwasabi.bigtong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigtongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
