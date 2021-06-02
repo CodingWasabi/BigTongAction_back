5d5d38c80b397d8b650929b3908e267e5cbe12dc
DESKTOP-NRTTKAM\daehwan <daehwan7989@naver.com>
2021-06-02 19:27:27 +0900

    #10 [feat] 방 생성하기

--- src/main/java/com/codingwasabi/bigtong/admin/controller/AdminController.java
+++ src/main/java/com/codingwasabi/bigtong/admin/controller/AdminController.java
@@ -1,12 +1,16 @@
 package com.codingwasabi.bigtong.admin.controller;
 
+import com.codingwasabi.bigtong.admin.entity.RoomType;
+import com.codingwasabi.bigtong.admin.service.ChatRoomService;
 import com.codingwasabi.bigtong.main.Account;
 import com.codingwasabi.bigtong.admin.service.AdminService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,11 +20,25 @@ import java.util.List;
 public class AdminController {
 
     private final AdminService adminService;
+    private final ChatRoomService chatRoomService;
 
     // 바꿔야함
     @GetMapping("/connecting")
-    public List<Account> showConnectiongUser(){
-        return adminService.userConnecting();
+    public String showConnectiongUser(){
+
+        return null;
+    }
+
+
+    @PostMapping("/create")
+    public String create_chatRoom(){
+
+        chatRoomService.createChatRoom("GRAIN");
+        chatRoomService.createChatRoom("VEGETABLE");
+        chatRoomService.createChatRoom("FRUIT");
+        chatRoomService.createChatRoom("MEAT");
+        chatRoomService.createChatRoom("FISH");
+        return "redirect:/";
     }
 
 }
