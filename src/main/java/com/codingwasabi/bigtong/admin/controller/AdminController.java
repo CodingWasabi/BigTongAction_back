a87f2d62f3cecd1e26ebf976b07a774c3c76db35
DESKTOP-NRTTKAM\daehwan <daehwan7989@naver.com>
2021-06-02 18:50:13 +0900

    css js 삭제

--- src/main/java/com/codingwasabi/bigtong/admin/controller/AdminController.java
+++ src/main/java/com/codingwasabi/bigtong/admin/controller/AdminController.java
@@ -3,21 +3,24 @@ package com.codingwasabi.bigtong.admin.controller;
 import com.codingwasabi.bigtong.main.Account;
 import com.codingwasabi.bigtong.admin.service.AdminService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/admin")
 public class AdminController {
 
     private final AdminService adminService;
 
+    // 바꿔야함
     @GetMapping("/connecting")
     public List<Account> showConnectiongUser(){
         return adminService.userConnecting();
     }
+
 }
